bb38164066051c4c6b56b68463b3b6c9b6a42699
Xue min <twer@xmyang.local>
2011-06-28 17:20:44 +0800

    Add rspec for google map api. xfwang&xmyang

--- Gemfile
+++ Gemfile
@@ -7,6 +7,11 @@ gem 'rails', '3.0.7'
 
 gem 'sqlite3'
 
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'webrat'
+end
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
---
+++ spec/google_map_api/spike_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "Google Map Directions API" do
+
+  before(:all) do
+    domain = "maps.googleapis.com"
+    path = "/maps/api/directions/json"
+    params = {:origin => "钟楼，西安", :destination => "鼓楼，西安", :sensor => "false"}
+    @json_result = http_get(domain, path, params)
+    @result = ActiveSupport::JSON.decode(@json_result)
+  end
+
+  it "should have distance" do
+    @result["routes"][0]["legs"][0].has_key?("distance").should be_true
+  end
+  
+  it "should have duration" do
+    @result["routes"][0]["legs"][0].has_key?("duration").should be_true
+  end
+    
+  it "should have start address" do
+    @result["routes"][0]["legs"][0].has_key?("start_address").should be_true
+  end
+
+  it "should have end address" do
+    @result["routes"][0]["legs"][0].has_key?("end_address").should be_true    
+  end
+  
+  it "should have steps" do 
+    @result["routes"][0]["legs"][0].has_key?("steps").should be_true
+  end
+
+  it "should have html_instructions in steps" do 
+    @result["routes"][0]["legs"][0]["steps"].first.has_key?("html_instructions").should be_true
+  end
+end
